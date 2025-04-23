ef19e3beeddda9a81e4d6d0faead8f63bedaed50
mrjuansebastian <itsjuansebastian@gmail.com>
2025-04-23 00:00:56 -0600

    Logica para administrar eventos.

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            eventos: [],
+            nuevoEvento: { nombre: '', hora: '', fecha: '' , lugar: '' },
+            editandoEvento: null
+        };
+    },
+    mounted() {
+        this.cargarEventos();
+    },
+    methods: {
+        async cargarEventos() {
+            try {
+                const response = await fetch('data.json');
+                this.usuarios = await response.json();
+            } catch (error) {
+                console.error('Error al cargar eventos:', error);
+                this.eventos = [];
+            }
+        },
+        async guardarEventos() {
+            try {
+                await fetch('data.json', {
+                    method: 'PUT', // O POST si el archivo no existe inicialmente
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(this.eventos)
+                });
+                console.log('eventos guardados correctamente.');
+            } catch (error) {
+                console.error('Error al guardar eventos:', error);
+            }
+        },
+        agregarEvento() {
+            this.eventos.push({ ...this.nuevoUsuario });
+            this.nuevoEvento = { nombre: '', hora: '', fecha: '' , lugar: '' };
+            this.guardarEventos();
+        },
+        editarEvento(index) {
+            this.editandoEvento = index;
+        },
+        guardarEdicion() {
+            this.editandoEvento = null;
+            this.guardarEventos();
+        },
+        cancelarEdicion() {
+            this.editandoEvento = null;
+        },
+        eliminarEvento(index) {
+            if (confirm('¿Estás seguro de eliminar este evento?')) {
+                this.eventos.splice(index, 1);
+                this.guardarEventos();
+            }
+        }
+    }
+}).mount('#app');
